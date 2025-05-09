e13bbcf34d835955b3f7b72338cc86ec2fdc1f81
suhanlee_localCom <kmjnjk1234@gmail.com>
2025-05-09 11:16:45 +0900

    Init

---
+++ TempleteServer/src/main/java/com/suhanlee/templeteserver/TempleteServerApplication.java
@@ -0,0 +1,13 @@
+package com.suhanlee.templeteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempleteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempleteServerApplication.class, args);
+    }
+
+}
---
+++ TempleteServer/src/test/java/com/suhanlee/templeteserver/TempleteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhanlee.templeteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempleteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
